6bba764ee967f38f6d658efb8a4ad8cba113d7cc
Ale <alejandromadrigal1409@gmail.com>
2026-05-18 21:10:08 -0600

    separacion en funciones, y creacion de outputs para programa plot y guardado automatico de graficas

--- single-intersection/Monte_Carlo_single_intersection.py
+++ single-intersection/Monte_Carlo_single_intersection.py
@@ -1,5 +1,5 @@
 import numpy as np
-from funciones import discretization
+from funciones import discretization, episode_generator, policy_evaluation, policy_improvement, plot_rewards
 import sumo_rl
 import gymnasium as gym
 import random
@@ -20,10 +20,12 @@ states = [
     for queue_w2e in ["low_queue", "medium_queue", "high_queue"]
 ]
 
+# Q(s,a) Values
 Q = {
     (s, a): 0.0 for s in states for a in actions
 }
 
+# Counter for incremental average of Q(s,a)
 N = {
     (s, a): 0 for s in states for a in actions
 }
@@ -33,16 +35,21 @@ policy = {
     (s, a): 1/len(actions) for s in states for a in actions
 }
 
+# creation of environment
 env = gym.make(
     'sumo-rl-v0',
     net_file="single-intersection.net.xml",
     route_file="single-intersection.rou.xml",
     use_gui=False,
-    num_seconds=1000
+    num_seconds=1000,
+    out_csv_name="outputs/montecarlo"
 )
 
-k = 0
+# Hyperameters
+k = 1 # flag to make policy improvment
+l = 5 # number of episodes before policy improvement
 
+# List to save the total reward of every episode
 reward_history = []
 
 for episode in range(episodes):
@@ -51,130 +58,26 @@ for episode in range(episodes):
     obs, info = env.reset()
 
     # initial state
-    state = discretization(obs)
-
-    episode_data = []
-
-    episode_reward = 0
+    initial_state = discretization(obs)
     
-
     # ===== GENERATE EPISODE =====
-    for step in range(1000):
-
-        # select action
-        probs = [policy[(state, action)] for action in actions]
-        action = random.choices(actions, weights=probs)[0]
-          
-
-        # execute action
-        next_obs, reward, terminated, truncated, info = env.step(action)
-
-        # episode finished?
-        done = terminated or truncated
-
-        # discretize next state
-        next_state = discretization(next_obs)
-
-        episode_reward += reward
-
-        # save transition
-        episode_data.append(
-            (state, action, reward)
-        )
-
-        # move to next state
-        state = next_state
-
-
-        if done:
-            break
+    episode_data, episode_reward = episode_generator(env, policy, initial_state, actions)
     
     reward_history.append(episode_reward)
 
-    # ===== TRACK EPISODE BACK =====
-    G = 0
-    
-    for t in reversed(range(len(episode_data))):
-        s, a, r = episode_data[t]
+    # ===== POLICY EVALUATION =====
 
-        G = gamma * G + r
+    Q, N = policy_evaluation(Q, N, gamma, episode_data)
 
-        # Incremental average
-        N[(s,a)] += 1
-        Q[(s, a)] += (G - Q[(s, a)]) / N[(s, a)]
-
-        '''
-        # select greedy action
-        q_values = [Q[(s, a)] for a in actions]
-        max_q = max(q_values)
-
-        best_actions = [a for a in actions if Q[(s, a)] == max_q]
-
-        best_action = best_actions[0]
-
-        for a in actions:
-            if a == best_action:
-                policy[(s,a)] = 1 - epsilon + epsilon/len(actions)
-            else:
-                policy[(s,a)] = epsilon/len(actions)
-        '''
-
-    if k == 3:
-
-        epsilon = max(0.01, epsilon * 0.999)
-
-        for s in states:
-            # select greedy action
-            q_values = [Q[(s, a)] for a in actions]
-            max_q = max(q_values)
-
-            best_actions = [a for a in actions if Q[(s, a)] == max_q]
-
-            best_action = random.choice(best_actions)
-
-            for a in actions:
-                if a == best_action:
-                    policy[(s,a)] = 1 - epsilon + epsilon/len(actions)
-                else:
-                    policy[(s,a)] = epsilon/len(actions)
+    # ===== POLICY IMPROVEMENT =====
+    if k == l:
+        policy = policy_improvement(states, actions, policy, Q, epsilon)
+        epsilon = max(0.01, epsilon * 0.995) # decreases the value of epsilon every l episodes
             
         k = 0
     else:
         k += 1
 
-
-
 env.close()
 
-
-import matplotlib.pyplot as plt
-
-# ===== MOVING AVERAGE =====
-avg_rewards = []
-
-window = 2
-
-for i in range(len(reward_history)):
-
-    start = max(0, i - window)
-
-    avg = np.mean(reward_history[start:i+1])
-
-    avg_rewards.append(avg)
-
-# ===== PLOT =====
-plt.figure(figsize=(12,6))
-
-plt.plot(reward_history, alpha=0.4, label="Episode Reward")
-
-plt.plot(avg_rewards, linewidth=2, label="Moving Average (100 episodes)")
-
-plt.xlabel("Episodes")
-plt.ylabel("Reward")
-plt.title("Monte Carlo Training Rewards")
-
-plt.legend()
-
-plt.grid(True)
-
-plt.show()
+plot_rewards(reward_history, window=2)
--- single-intersection/funciones.py
+++ single-intersection/funciones.py
@@ -1,5 +1,7 @@
-import traci
 import numpy as np
+import sumo_rl
+import gymnasium as gym
+import random
 
 def discretization(obs):
 
@@ -15,22 +17,22 @@ def discretization(obs):
         if value < 0.3:
             return f"low_{category_type}"
 
-        elif value < 1.5:
+        elif value < 0.7:
             return f"medium_{category_type}"
 
         else:
             return f"high_{category_type}"
 
     # ===== DENSITIES =====
-    density_n2s = obs[3] + obs[4]
-    density_w2e = obs[5] + obs[6]
+    density_n2s = (obs[3] + obs[4]) / 2
+    density_w2e = (obs[5] + obs[6]) / 2
 
     density_n2s = categorize(density_n2s, "density")
     density_w2e = categorize(density_w2e, "density")
 
     # ===== QUEUES =====
-    queue_n2s = obs[7] + obs[8]
-    queue_w2e = obs[9] + obs[10]
+    queue_n2s = (obs[7] + obs[8]) / 2
+    queue_w2e = (obs[9] + obs[10]) / 2
 
     queue_n2s = categorize(queue_n2s, "queue")
     queue_w2e = categorize(queue_w2e, "queue")
@@ -44,5 +46,128 @@ def discretization(obs):
         queue_n2s,
         queue_w2e
     )
+
+def episode_generator(env, policy, state, actions):
+    episode_data = []
+    episode_reward = 0
+    while True:
+
+        # select action
+        probs = [policy[(state, action)] for action in actions]
+        action = random.choices(actions, weights=probs)[0]
+          
+
+        # execute action
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # episode finished?
+        done = terminated or truncated
+
+        # discretize next state
+        next_state = discretization(next_obs)
+
+        episode_reward += reward
+
+        # save transition
+        episode_data.append((state, action, reward))
+
+        # move to next state
+        state = next_state
+
+
+        if done:
+            break
+    
+    return episode_data, episode_reward
+
+def policy_evaluation(Q, N, gamma, episode_data):
+    # ===== TRACK EPISODE BACK =====
+    G = 0
+    
+    for t in reversed(range(len(episode_data))):
+        s, a, r = episode_data[t]
+
+        G = gamma * G + r
+
+        # Incremental average
+        N[(s,a)] += 1
+        Q[(s, a)] += (G - Q[(s, a)]) / N[(s, a)]
+
+    return Q, N
+
+
+def policy_improvement(states, actions, policy, Q, epsilon):
+    for s in states:
+        q_values = [Q[(s, a)] for a in actions] # get Q(s,a) for every a in state s
+        max_q = max(q_values) # get greedy Q(s,a) in state s
+
+        best_actions = [a for a in actions if Q[(s, a)] == max_q] # checks which a in state s has Q(s,a) equal to max_q
+
+        best_action = random.choice(best_actions) # selects best action
+
+        # Policy improvement
+        for a in actions:
+            if a == best_action:
+                policy[(s,a)] = 1 - epsilon + epsilon/len(actions)
+            else:
+                policy[(s,a)] = epsilon/len(actions)
+        
+    return policy
+    
+import os
+import matplotlib.pyplot as plt
+
+
+def plot_rewards(reward_history, window=10, save_dir="graphs"):
+
+    # ===== CREATE FOLDER IF IT DOESN'T EXIST =====
+    os.makedirs(save_dir, exist_ok=True)
+
+    # ===== MOVING AVERAGE =====
+    avg_rewards = []
+
+    for i in range(len(reward_history)):
+
+        start = max(0, i - window)
+
+        avg = np.mean(reward_history[start:i+1])
+
+        avg_rewards.append(avg)
+
+    # ===== PLOT =====
+    plt.figure(figsize=(12,6))
+
+    plt.plot(
+        reward_history,
+        alpha=0.4,
+        label="Episode Reward"
+    )
+
+    plt.plot(
+        avg_rewards,
+        linewidth=2,
+        label=f"Moving Average ({window} episodes)"
+    )
+
+    plt.xlabel("Episodes")
+    plt.ylabel("Reward")
+    plt.title("Monte Carlo Training Rewards")
+
+    plt.legend()
+
+    plt.grid(True)
+
+    # ===== SAVE FIGURE =====
+    save_path = os.path.join(
+        save_dir,
+        "training_rewards.png"
+    )
+
+    plt.savefig(save_path, dpi=300, bbox_inches="tight")
+
+    # ===== CLOSE FIGURE =====
+    plt.close()
+
+    print(f"Graph saved in: {save_path}")
     
 
