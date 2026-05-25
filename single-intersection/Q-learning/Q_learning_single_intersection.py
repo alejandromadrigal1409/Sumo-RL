7d22e6babac01c7a93a68b6617200e55e377dc7e
Ale <alejandromadrigal1409@gmail.com>
2026-05-25 15:53:03 -0600

    modificaciones para q learning

---
+++ single-intersection/Q-learning/Q_learning_single_intersection.py
@@ -0,0 +1,120 @@
+import numpy as np
+from funciones import discretization, episode_generator, policy_evaluation, policy_improvement, save_experiment
+import sumo_rl
+import gymnasium as gym
+import random
+
+actions = [0, 1]
+
+states = [
+    (phase, flag, den_n2s, den_w2e, queue_n2s, queue_w2e)
+    for phase in ["GGrr", "rrGG"]
+    for flag in [0, 1]
+    for den_n2s in ["low_density", "medium_density", "high_density"]
+    for den_w2e in ["low_density", "medium_density", "high_density"]
+    for queue_n2s in ["low_queue", "medium_queue", "high_queue"]
+    for queue_w2e in ["low_queue", "medium_queue", "high_queue"]
+]
+
+import yaml
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+gamma = config["gamma"]
+episodes = config["episodes"]
+num_seconds = config["num_seconds"]
+epsilon_min = config["epsilon"]["min"]
+epsilon_decay = config["epsilon"]["decay"]
+policy_update_interval = config["policy_update_interval"]
+delta_time = config["delta_time"]
+method = config["method"]
+master_seed = config["master_seed"]
+num_trainings = config["num_trainings"]
+alpha = config["alpha"]
+
+# ====== MAIN CODE =========
+
+random.seed(master_seed)
+
+seeds = random.sample(range(1, 100000),num_trainings)
+
+# List to save rewards of EVERY training
+all_training_rewards = []
+
+best_reward = -np.inf
+best_policy = None
+
+for seed in seeds:
+    epsilon = config["epsilon"]["start"]
+    random.seed(seed)
+    np.random.seed(seed)
+
+    # List to save the total reward of every episode on each training
+    training_rewards  = []
+
+    # Q(s,a) Values
+    Q = {(s, a): 0.0 for s in states for a in actions}
+
+    # Counter for incremental average of Q(s,a)
+    N = {(s, a): 0 for s in states for a in actions}
+
+    # epsilon greedy policy
+    policy = {(s, a): 1/len(actions) for s in states for a in actions}
+
+    # creation of environment
+    env = gym.make(
+        'sumo-rl-v0',
+        net_file="../single-intersection.net.xml",
+        route_file="../single-intersection.rou.xml",
+        #out_csv_name="outputs/montecarlo", generates .cvs files for each episode
+        delta_time = delta_time,
+        use_gui=False,
+        num_seconds=num_seconds,
+    )
+
+    for episode in range(episodes):
+
+        # reset environment
+        obs, info = env.reset(seed=seed + episode)
+
+        # initial state
+        initial_state = discretization(obs)
+        
+        # ===== GENERATE EPISODE =====
+        Q = episode_generator(env, initial_state, actions, gamma, alpha, Q)
+        
+        training_rewards.append(episode_reward)
+
+        # ===== POLICY EVALUATION =====
+        Q, N = policy_evaluation(Q, N, gamma, episode_data, method)
+
+        # ===== POLICY IMPROVEMENT =====
+        if (episode + 1) % policy_update_interval == 0:
+            policy = policy_improvement(states, actions, policy, Q, epsilon)
+            # decreases the value of epsilon every policy_update_interval episodes
+            epsilon = max(
+                epsilon_min,
+                epsilon * epsilon_decay
+            )
+                
+    mean_training_reward = np.mean(training_rewards)
+
+    if mean_training_reward > best_reward:
+        best_reward = mean_training_reward
+        best_policy = policy.copy()
+
+    all_training_rewards.append(training_rewards)
+
+
+    env.close()
+
+mean_all_training_rewards = np.mean(all_training_rewards, axis = 0)
+
+save_experiment(
+    reward_history=mean_all_training_rewards,
+    all_training_rewards=all_training_rewards,
+    best_policy=best_policy,
+    config=config,
+    seeds=seeds
+)
+
---
+++ single-intersection/Q-learning/funciones.py
@@ -0,0 +1,193 @@
+import random
+
+def discretization(obs):
+
+    # ===== PHASE =====
+    phase = "GGrr" if obs[0] == 1 else "rrGG"
+
+    # ===== MIN_GREEN FLAG =====
+    min_green_flag = int(obs[2])
+
+    # ===== AUXILIARY FUNCTION =====
+    def categorize(value, category_type):
+
+        if value < 0.3:
+            return f"low_{category_type}"
+
+        elif value < 0.7:
+            return f"medium_{category_type}"
+
+        else:
+            return f"high_{category_type}"
+
+    # ===== DENSITIES =====
+    density_n2s = (obs[3] + obs[4]) / 2
+    density_w2e = (obs[5] + obs[6]) / 2
+
+    density_n2s = categorize(density_n2s, "density")
+    density_w2e = categorize(density_w2e, "density")
+
+    # ===== QUEUES =====
+    queue_n2s = (obs[7] + obs[8]) / 2
+    queue_w2e = (obs[9] + obs[10]) / 2
+
+    queue_n2s = categorize(queue_n2s, "queue")
+    queue_w2e = categorize(queue_w2e, "queue")
+
+    # ===== DISCRETE STATE =====
+    return (
+        phase,
+        min_green_flag,
+        density_n2s,
+        density_w2e,
+        queue_n2s,
+        queue_w2e
+    )
+
+def choose_action(state, epsilon, actions, Q):
+    if random.random < epsilon:
+        return random.choice(actions)
+     
+    q_vals = [Q[(state,a)] for a in actions]
+    q_max = max(q_vals)
+
+    best_actions = [act for act in actions if Q[(state, act)] == q_max]
+    return random.choice(best_actions)
+
+def episode_generator(env, state, actions, gamma, alpha, Q):
+    episode_data = []
+    episode_reward = 0
+    while True:
+
+        # select action
+        action = choose_action(state)
+          
+        # execute action
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # episode finished?
+        done = terminated or truncated
+
+        # discretize next state
+        next_state = discretization(next_obs)
+
+        episode_reward += reward
+
+        # save transition
+        episode_data.append((state, action, reward))
+
+        max_q = max(Q[(next_state, act)] for act in actions)
+
+        Q[(state, action)] += alpha * (reward + (gamma * max_q) - Q[(state, action)])
+
+        # move to next state
+        state = next_state
+
+        if done:
+            break
+    
+    return Q
+    
+from datetime import datetime
+import os
+import matplotlib.pyplot as plt
+import pickle
+
+
+def save_experiment(
+    reward_history,
+    all_training_rewards,
+    best_policy,
+    config,
+    seeds,
+    base_dir="experiments"
+):
+    method = "FV" if config["method"] == "first_visit" else "EV"
+
+    # ===== CURRENT DATE/TIME =====
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # ===== CREATE EXPERIMENT FOLDER =====
+    save_dir = os.path.join(
+        base_dir,
+        f"MC_{method}_experiment_{timestamp}"
+    )
+
+    os.makedirs(save_dir, exist_ok=True)
+
+    # =========================================================
+    # ===================== SAVE PLOT =========================
+    # =========================================================
+
+    plt.figure(figsize=(12,6))
+
+    plt.plot(
+        reward_history,
+        label="Episode Reward"
+    )
+
+    plt.xlabel("Episodes")
+    plt.ylabel("Reward")
+    plt.title("Monte Carlo Training Rewards")
+
+    plt.legend()
+    plt.grid(True)
+
+    plot_path = os.path.join(
+        save_dir,
+        f"MC_{method}_training_rewards_{timestamp}.png"
+    )
+
+    plt.savefig(
+        plot_path,
+        dpi=300,
+        bbox_inches="tight"
+    )
+
+    plt.close()
+
+    # =========================================================
+    # ================= SAVE CONFIG YAML ======================
+    # =========================================================
+
+    import yaml
+
+    config_to_save = config.copy()
+
+    config_to_save["generated_seeds"] = seeds
+
+    config_path = os.path.join(
+        save_dir,
+        f"MC_{method}_experiment_config_{timestamp}.yaml"
+    )
+
+    with open(config_path, "w") as f:
+        yaml.dump(
+            config_to_save,
+            f,
+            sort_keys=False
+        )
+
+    # =========================================================
+    # ================= SAVE DATA ======================
+    # =========================================================
+
+    # Save policy
+    policy_path = os.path.join(
+        save_dir,
+        f"best_policy_{timestamp}.pkl"
+    )
+
+    with open(policy_path, "wb") as f:
+        pickle.dump(best_policy, f)
+
+    # Save data of all trainnings
+    all_training_rewards_path = os.path.join(
+        save_dir,
+        f"all_training_rewards_{timestamp}.pkl"
+    )
+
+    with open(all_training_rewards_path, "wb") as f:
+        pickle.dump(all_training_rewards, f)
+    
+
